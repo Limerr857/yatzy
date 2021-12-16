6d579d617d1e5aa9074d96ae20223754e6d64d76
it <it@PROG-IT-17>
2021-12-16 14:53:37 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Yatzy_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxFramtid = new System.Windows.Forms.GroupBox();
+            this.pnlFramtid = new System.Windows.Forms.Panel();
+            this.gbxInstallningar = new System.Windows.Forms.GroupBox();
+            this.gbxSpel = new System.Windows.Forms.GroupBox();
+            this.btnRulla = new System.Windows.Forms.Button();
+            this.pctTärning5 = new System.Windows.Forms.PictureBox();
+            this.pctTärning4 = new System.Windows.Forms.PictureBox();
+            this.pctTärning3 = new System.Windows.Forms.PictureBox();
+            this.pctTärning2 = new System.Windows.Forms.PictureBox();
+            this.pctTärning1 = new System.Windows.Forms.PictureBox();
+            this.lbxFramtid = new System.Windows.Forms.ListBox();
+            this.gbxFramtid.SuspendLayout();
+            this.pnlFramtid.SuspendLayout();
+            this.gbxSpel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxFramtid
+            // 
+            this.gbxFramtid.Controls.Add(this.pnlFramtid);
+            this.gbxFramtid.Location = new System.Drawing.Point(328, 12);
+            this.gbxFramtid.Name = "gbxFramtid";
+            this.gbxFramtid.Size = new System.Drawing.Size(199, 264);
+            this.gbxFramtid.TabIndex = 0;
+            this.gbxFramtid.TabStop = false;
+            this.gbxFramtid.Text = "Möjliga kombinationer";
+            // 
+            // pnlFramtid
+            // 
+            this.pnlFramtid.AutoScroll = true;
+            this.pnlFramtid.Controls.Add(this.lbxFramtid);
+            this.pnlFramtid.Location = new System.Drawing.Point(6, 19);
+            this.pnlFramtid.Name = "pnlFramtid";
+            this.pnlFramtid.Size = new System.Drawing.Size(187, 239);
+            this.pnlFramtid.TabIndex = 1;
+            // 
+            // gbxInstallningar
+            // 
+            this.gbxInstallningar.Location = new System.Drawing.Point(12, 194);
+            this.gbxInstallningar.Name = "gbxInstallningar";
+            this.gbxInstallningar.Size = new System.Drawing.Size(310, 82);
+            this.gbxInstallningar.TabIndex = 1;
+            this.gbxInstallningar.TabStop = false;
+            this.gbxInstallningar.Text = "Inställningar";
+            // 
+            // gbxSpel
+            // 
+            this.gbxSpel.Controls.Add(this.btnRulla);
+            this.gbxSpel.Controls.Add(this.pctTärning5);
+            this.gbxSpel.Controls.Add(this.pctTärning4);
+            this.gbxSpel.Controls.Add(this.pctTärning3);
+            this.gbxSpel.Controls.Add(this.pctTärning2);
+            this.gbxSpel.Controls.Add(this.pctTärning1);
+            this.gbxSpel.Location = new System.Drawing.Point(12, 12);
+            this.gbxSpel.Name = "gbxSpel";
+            this.gbxSpel.Size = new System.Drawing.Size(310, 176);
+            this.gbxSpel.TabIndex = 2;
+            this.gbxSpel.TabStop = false;
+            this.gbxSpel.Text = "Spel";
+            // 
+            // btnRulla
+            // 
+            this.btnRulla.Location = new System.Drawing.Point(110, 75);
+            this.btnRulla.Name = "btnRulla";
+            this.btnRulla.Size = new System.Drawing.Size(75, 23);
+            this.btnRulla.TabIndex = 5;
+            this.btnRulla.Text = "Rulla!";
+            this.btnRulla.UseVisualStyleBackColor = true;
+            this.btnRulla.Click += new System.EventHandler(this.btnRulla_Click);
+            // 
+            // pctTärning5
+            // 
+            this.pctTärning5.Image = global::Yatzy_1.tärningsgrafik.d0;
+            this.pctTärning5.Location = new System.Drawing.Point(238, 19);
+            this.pctTärning5.Name = "pctTärning5";
+            this.pctTärning5.Size = new System.Drawing.Size(52, 50);
+            this.pctTärning5.TabIndex = 4;
+            this.pctTärning5.TabStop = false;
+            // 
+            // pctTärning4
+            // 
+            this.pctTärning4.Image = global::Yatzy_1.tärningsgrafik.d0;
+            this.pctTärning4.Location = new System.Drawing.Point(180, 19);
+            this.pctTärning4.Name = "pctTärning4";
+            this.pctTärning4.Size = new System.Drawing.Size(52, 50);
+            this.pctTärning4.TabIndex = 3;
+            this.pctTärning4.TabStop = false;
+            // 
+            // pctTärning3
+            // 
+            this.pctTärning3.Image = global::Yatzy_1.tärningsgrafik.d0;
+            this.pctTärning3.Location = new System.Drawing.Point(122, 19);
+            this.pctTärning3.Name = "pctTärning3";
+            this.pctTärning3.Size = new System.Drawing.Size(52, 50);
+            this.pctTärning3.TabIndex = 2;
+            this.pctTärning3.TabStop = false;
+            // 
+            // pctTärning2
+            // 
+            this.pctTärning2.Image = global::Yatzy_1.tärningsgrafik.d0;
+            this.pctTärning2.Location = new System.Drawing.Point(64, 19);
+            this.pctTärning2.Name = "pctTärning2";
+            this.pctTärning2.Size = new System.Drawing.Size(52, 50);
+            this.pctTärning2.TabIndex = 1;
+            this.pctTärning2.TabStop = false;
+            // 
+            // pctTärning1
+            // 
+            this.pctTärning1.BackColor = System.Drawing.SystemColors.Control;
+            this.pctTärning1.Image = global::Yatzy_1.tärningsgrafik.d0;
+            this.pctTärning1.Location = new System.Drawing.Point(6, 19);
+            this.pctTärning1.Name = "pctTärning1";
+            this.pctTärning1.Size = new System.Drawing.Size(52, 50);
+            this.pctTärning1.TabIndex = 0;
+            this.pctTärning1.TabStop = false;
+            // 
+            // lbxFramtid
+            // 
+            this.lbxFramtid.FormattingEnabled = true;
+            this.lbxFramtid.Location = new System.Drawing.Point(3, 3);
+            this.lbxFramtid.Name = "lbxFramtid";
+            this.lbxFramtid.Size = new System.Drawing.Size(120, 95);
+            this.lbxFramtid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 288);
+            this.Controls.Add(this.gbxSpel);
+            this.Controls.Add(this.gbxInstallningar);
+            this.Controls.Add(this.gbxFramtid);
+            this.Name = "Form1";
+            this.Text = "Yatzy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxFramtid.ResumeLayout(false);
+            this.pnlFramtid.ResumeLayout(false);
+            this.gbxSpel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTärning1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxFramtid;
+        private System.Windows.Forms.Panel pnlFramtid;
+        private System.Windows.Forms.GroupBox gbxInstallningar;
+        private System.Windows.Forms.GroupBox gbxSpel;
+        private System.Windows.Forms.PictureBox pctTärning5;
+        private System.Windows.Forms.PictureBox pctTärning4;
+        private System.Windows.Forms.PictureBox pctTärning3;
+        private System.Windows.Forms.PictureBox pctTärning2;
+        private System.Windows.Forms.PictureBox pctTärning1;
+        private System.Windows.Forms.Button btnRulla;
+        private System.Windows.Forms.ListBox lbxFramtid;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yatzy_1
+{
+    public partial class Form1 : Form
+    {
+        // Viktiga variabler som kommer att användas genom hela programmet
+        // Skapa en array som innehåller tärningsgrafiken
+        Image[] tärningarGrafik = new Image[] { tärningsgrafik.d0, tärningsgrafik.d1,
+                                                tärningsgrafik.d2, tärningsgrafik.d3,
+                                                tärningsgrafik.d4, tärningsgrafik.d5,
+                                                tärningsgrafik.d6 };
+
+        // Skapa en array som innehåller tärningarnas värde
+        // Siffran som visas på tärningen 0 = ingen siffra
+        int[] tärningarVärde = new int[5];
+
+        // Skapa en array som innehåller tärningarnas status
+        // 0 = ej vald, 1 = vald, 2 = rullar
+        int[] tärningarStatus = new int[5];
+
+        // Skapa en slumpgenerator
+        Random rand = new Random();
+
+        // Skapa en array med alla färger i KnownColor
+        KnownColor[] färgnamn = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+
+        // Skapa en lista med array:er av alla tidigare tärningsslag
+        List<Array> historikLista = new List<Array>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRulla_Click(object sender, EventArgs e)
+        {
+            KastaTärningarna(tärningarGrafik, tärningarVärde, historikLista);
+
+            UppdateraGrafik(tärningarGrafik, tärningarVärde);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LaddaSpelet(tärningarVärde, tärningarStatus, tärningarGrafik, historikLista);
+        }
+
+        private void KastaTärningarna(Image[] tärningarGrafik, int[] tärningarVärde, List<Array> historikLista)
+        {
+            // Sätt alla tärningars status till "Rullar" 
+            for (int i = 0; i < 5; i++)
+            {
+                tärningarStatus[i] = 2;
+            }
+
+            // Stäng av "Rulla!" knappen
+            btnRulla.Enabled = false;
+
+            // Loopen kan starta
+            bool rullar = true;
+            // Starta en loop med tärningsgrafik
+            while (rullar)
+            {
+
+                // Om alla tärningar har slutat rulla, stoppa loopen
+                if (!tärningarStatus.Contains(2))
+                    {
+                        rullar = false;
+                    }
+
+                // Byt nummer på alla tärningar slumpmässigt
+                for (int i = 0; i < 5; i++)
+                {
+                    // Men bara om tärningen har statusen "Rullar"
+                    if (tärningarStatus[i] == 2)
+                    {
+                        tärningarVärde[i] = rand.Next(1, 7);
+                        SlumpaTärningsfärg(false, tärningarStatus);
+                    }               
+                }
+
+                // Uppdatera tärningsgrafiken
+                UppdateraGrafik(tärningarGrafik, tärningarVärde);
+
+                // Vänta en liten stund
+                Thread.Sleep(50);
+
+                // Sluta rulla den första rullande tärningen med 1/10 chans varje "frame"
+                if (rand.Next(10) == 9)
+                {
+                    // Iterera över alla tärningars status
+                    for (int i = 0; i < 5; i++)
+
+                    {
+                        // Om tärningen har statusen rullar
+                        if (tärningarStatus[i] == 2)
+                        {
+                            // Stoppa tärningen från att rulla
+                            tärningarStatus[i] = 0;
+
+                            // Bryt for-loopen
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Sätt på "Rulla!" knappen efter tärningarna har rullats
+            btnRulla.Enabled = true;
+
+            // Och återställ tärningarnas bakgrundsfärg
+            SlumpaTärningsfärg(true, tärningarStatus);
+
+            //
+            // Poängdelen
+            //
+
+            // Lägg till resultatet i historiken
+            historikLista.Add(tärningarVärde);
+            
+        }
+
+        private void UppdateraGrafik(Image[] tärningarGrafik, int[] tärningarVärde)
+        {
+            // Sätt varje tärnings bild till dess värde
+            pctTärning1.Image = tärningarGrafik[tärningarVärde[0]];
+            pctTärning2.Image = tärningarGrafik[tärningarVärde[1]];
+            pctTärning3.Image = tärningarGrafik[tärningarVärde[2]];
+            pctTärning4.Image = tärningarGrafik[tärningarVärde[3]];
+            pctTärning5.Image = tärningarGrafik[tärningarVärde[4]];
+
+            // Tvinga alla tärningar att uppdatera sina bilder
+            pctTärning1.Refresh();
+            pctTärning2.Refresh();
+            pctTärning3.Refresh();
+            pctTärning4.Refresh();
+            pctTärning5.Refresh();
+        }
+
+        private void LaddaSpelet(int[] tärningarVärde, int[] tärningarStatus, Image[] tärningarGrafik, List<Array> historikLista)
+        {
+            // Återställ array:en som innehåller tärningarnas värde
+            // Siffran som visas på tärningen 0 = ingen siffra
+            for (int i = 0; i < tärningarVärde.Length; i++)
+            {
+                tärningarVärde[i] = 0;
+            }
+
+            // Återställ array:en som innehåller tärningarnas status
+            // 0 = ej vald, 1 = vald, 2 = rulla
+            for (int i = 0; i < tärningarStatus.Length; i++)
+            {
+               
+                tärningarStatus[i] = 0;
+            }
+
+            // Återställ tärningsgrafiken
+            UppdateraGrafik(tärningarGrafik, tärningarVärde);
+
+            // Återställ tärningshistoriken
+            historikLista.Clear();
+            
+        }
+
+        private void SlumpaTärningsfärg(bool återställ, int[] tärningarStatus)
+        {
+            // Om färgerna ska återställas, gör det och avsluta
+            if (återställ)
+            {
+                pctTärning1.BackColor = Control.DefaultBackColor;
+                pctTärning2.BackColor = Control.DefaultBackColor;
+                pctTärning3.BackColor = Control.DefaultBackColor;
+                pctTärning4.BackColor = Control.DefaultBackColor;
+                pctTärning5.BackColor = Control.DefaultBackColor;
+                return;
+            }
+
+            // Ger alla tärningarna en bakgrundsfärg slumpmässigt
+            if (tärningarStatus[0] == 2)
+            {
+                pctTärning1.BackColor = Color.FromKnownColor(färgnamn[rand.Next(färgnamn.Length)]);
+            }
+            if (tärningarStatus[1] == 2)
+            {
+                pctTärning2.BackColor = Color.FromKnownColor(färgnamn[rand.Next(färgnamn.Length)]);
+            }
+            if (tärningarStatus[2] == 2)
+            {
+                pctTärning3.BackColor = Color.FromKnownColor(färgnamn[rand.Next(färgnamn.Length)]);
+            }
+            if (tärningarStatus[3] == 2)
+            {
+                pctTärning4.BackColor = Color.FromKnownColor(färgnamn[rand.Next(färgnamn.Length)]);
+            }
+            if (tärningarStatus[4] == 2)
+            {
+                pctTärning5.BackColor = Color.FromKnownColor(färgnamn[rand.Next(färgnamn.Length)]);
+            }
+            
+            // Avsluta metoden
+            return;
+        }
+
+
+        private List<string> MöjligaSpecialfall(int[] tempTärningarVärde)
+        {
+            // Metoden ger en lista med alla möjliga specialfall
+            // utifrån en viss samling tärningsvärden
+
+            // Skapa en lista som sparar de möjliga specialfallen
+            List<string> specialfall = new List<string>();
+
+            // Skapa en double som mäter hur många gånger den
+            // vanligaste siffran finns i tempTärningarVärde
+            double vanligastRäkna = 0;
+
+            // Och en double som sparar värdet på siffran
+            double vanligastSiffra = 0;
+
+            // Och beräkna båda variablarnas värden
+            for (int i = 0; i < tempTärningarVärde.Count(); i++)
+            {
+                // Om det finns fler av *i* inuti tempTärningarVärde än tidigare *i*
+                if (tempTärningarVärde.Count(c => c == tempTärningarVärde[i]) > vanligastRäkna)
+                {
+                    // Spara hur många gånger *i* finns inuti tempTärningarVärde
+                    vanligastRäkna = tempTärningarVärde.Count(c => c == tempTärningarVärde[i]);
+
+                    // Spara värdet på *i*
+                    vanligastSiffra = tempTärningarVärde[i];
+                }
+            }
+
+            // Samma sak som vanligast fast med den andra vanligaste siffran
+            double andraVanligastRäkna = 0;
+            double andraVanligastSiffra;
+
+            // Skapa en ny array från tempTärningarVärde utan den vanligaste siffran och
+            // kopiera innehållet av tempTärningarVärde till tempTärningarVärdeAndra
+            int[] tempTärningarVärdeAndra = (int[])tempTärningarVärde.Clone();
+
+            // Ta bort den vanligaste siffran
+            tempTärningarVärdeAndra = tempTärningarVärdeAndra.Where(val => val != vanligastSiffra).ToArray();
+
+            // Beräkna båda variablarnas värden
+            for (int i = 0; i < tempTärningarVärdeAndra.Count(); i++)
+            {
+                // Om det finns fler av *i* inuti tempTärningarVärdeAndra än tidigare *i*
+                if (tempTärningarVärdeAndra.Count(c => c == tempTärningarVärdeAndra[i]) > andraVanligastRäkna)
+                {
+                    // Spara hur många gånger *i* finns inuti tempTärningarVärdeAndra
+                    andraVanligastRäkna = tempTärningarVärdeAndra.Count(c => c == tempTärningarVärdeAndra[i]);
+
+                    // Spara värdet på *i*
+                    andraVanligastSiffra = tempTärningarVärdeAndra[i];
+                }
+            }
+
+            // Skapa en bool som kollar om alla if-satser nedan är falska
+            bool ingaSpecial = true;
+
+            // Om det har blivit Yatzy
+            if (vanligastRäkna == 5)
+            {
+                specialfall.Add("yatzy");
+                ingaSpecial = false;
+            }
+
+            // Om det blivit fyrtal 
+            if (vanligastRäkna == 4)
+            {
+                specialfall.Add("fyrtal");
+                ingaSpecial = false;
+            }
+
+            // Om det blivit tretal
+            if (vanligastRäkna == 3)
+            {
+                specialfall.Add("tretal");
+                ingaSpecial = false;
+            }
+
+            // Om det blivit kåk (3 + 2 lika)
+            if (vanligastRäkna == 3 && andraVanligastRäkna == 2)
+            {
+                specialfall.Add("kåk");
+                ingaSpecial = false;
+            }
+
+            // Om det blivit ett par (2 lika)
+            if (vanligastRäkna == 2)
+            {
+                specialfall.Add("ett par");
+                ingaSpecial = false;
+            }
+
+            // Om det blivit två par (2 + 2 lika)
+            if (vanligastRäkna == 2 && andraVanligastRäkna == 2)
+            {
+                specialfall.Add("två par");
+                ingaSpecial = false;
+            }
+
+            // Om inga andra specialfall har satts, ge en sista chans enligt spelreglerna
+            if (ingaSpecial)
+            {
+                specialfall.Add("chans");
+            }
+
+            // return poäng, specialfall
+            return specialfall;
+        }
+    }
+}
+
